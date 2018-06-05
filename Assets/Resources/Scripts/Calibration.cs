2a26f391381664fb41eb3e2cf929bf871cdee0e2
Brandon Lacy <blacy1551@gmail.com>
2018-06-05 17:19:10 -0500

    Created a block measurement systemq
    The player pushes blocks away from they hand to figure out their maximum/minimium arm range. works for both hands.

--- Assets/Resources/Scripts/Calibration.cs
+++ Assets/Resources/Scripts/Calibration.cs
@@ -5,42 +5,82 @@ using UnityEngine.UI;
 
 public class Calibration : MonoBehaviour {
 
-	private string currentStage = "player"; //player, leftArmVertical, leftArmHorizontal, rightArmVertical, rightArmHorizontal
-
+    private int currentStage = 0; //0 - pos, 1 - leftVerticalMin, 2 - leftVerticalMax
 	public GameObject player;
 	public Transform correctPlayerPos;
-	 
 
+    public Transform avatar;
+    public Transform leftHand;
+    public Transform rightHand;
+
+
+    public GameObject cubeHolder;
+    public Transform leftCubeSpot;
+    public Transform rightCubeSpot;
+    
 	public Text tv;
 	// Use this for initialization
 	void Start () 
 	{
-		tv.text = "Please move to the blue square on the ground and press a button.";
-	}
+		tv.text = "Please move to the blue square on the ground. (Press a button to <continue>)";
+        //cubeHolder.gameObject.SetActive(false);
+    }
   	
 	void Update()
 	{
-		if (OVRInput.Get (OVRInput.RawButton.LIndexTrigger) || OVRInput.Get (OVRInput.RawButton.RIndexTrigger) || Input.GetKey(KeyCode.Space)) {
-			switch (currentStage) {
-			case "player":
-				//Debug.Log (Vector3.Distance (player.transform.position, correctPlayerPos.position));
-				if (getPlayerDistance() <= 0.07){
-					tv.text = "Next we will test your left arm's maximum vertical movement.";
-					currentStage = "leftArmVertical";
-				}
+        if (OVRInput.GetUp(OVRInput.RawButton.LIndexTrigger) || OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger) || Input.GetKeyUp(KeyCode.Space))
+        {
+            switch (currentStage)
+            {
+                case 0:
+                    //Debug.Log (Vector3.Distance (player.transform.position, correctPlayerPos.position));
+                    if (getDistance(player.transform, correctPlayerPos) <= 0.2 && getDistance(player.transform, correctPlayerPos) >= -0.2)
+                    {
+                        tv.text = "Next we will check the bounds for your left hand. Start by putting your LEFT hand inside the box.";
+                        Instantiate(cubeHolder, leftCubeSpot.transform.position, Quaternion.identity);
 
-				break;
-			case "leftArmVertical":
+                        rightHand.gameObject.SetActive(false);
+                        currentStage++;
+                    }
+
+                    break;
+
+                case 2:
+                    tv.text = "OK, next we are going to repeat that with the other hand. Place your RIGHT hand in the box.";
+                    GameObject prev = GameObject.Find("CubeHolder(Clone)");
+                    Destroy(prev);
+                    Instantiate(cubeHolder, rightCubeSpot.transform.position, Quaternion.identity);
+                    
+                    rightHand.gameObject.SetActive(true);
+                    leftHand.gameObject.SetActive(false);
+                    currentStage++;
+                    break;
+                case 4:
+                    tv.text = "Done for now!";
+                    GameObject prev1 = GameObject.Find("CubeHolder(Clone)");
+                    Destroy(prev1);
+                    rightHand.gameObject.SetActive(true);
+                    leftHand.gameObject.SetActive(false);
+                    break;
+            }
+            
+        }
+        if (Vector3.Distance(leftHand.position, cubeHolder.transform.position) <= 0.2 && currentStage == 1)
+        {
+            tv.text = "Now push the little boxes away from your LEFT hand.";
+            currentStage++;
+        }
+        if (Vector3.Distance(rightHand.position, cubeHolder.transform.position) <= 0.2 && currentStage == 3)
+        {
+            tv.text = "Now push the little boxes away from your RIGHT hand.";
+            currentStage++;
+        }
+    }
 
 
-				break;
-			}
-		}
-	}
-
-	float getPlayerDistance()
+	float getDistance(Transform x1, Transform x2)
 	{
-		Vector3 vectorToTarget = player.transform.position - correctPlayerPos.position;
+		Vector3 vectorToTarget = x1.position - x2.position;
 		vectorToTarget.y = 0;
 		float distanceToTarget = vectorToTarget.magnitude;
 		Debug.Log (distanceToTarget);
---
+++ Assets/Resources/Scripts/PushBlock.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushBlock : MonoBehaviour {
+
+    private float left = 0f;
+    private float right = 0f;
+    private float up = 0f;
+    private float down = 0f;
+    private float forward = 0f;
+
+    public Text tv;
+    public Transform cubeParent;
+     
+    void Start()
+    {
+        
+        
+    }
+    private void OnCollisionExit(Collision col)
+    {
+
+        if (col.gameObject.name.Equals("Left"))
+        {
+            float dis = getDistance("left", col.transform);
+            left = normalize(dis, 0, 0.5f); //0.07
+
+        }
+        else if (col.gameObject.name.Equals("Right"))
+        {
+            float dis = getDistance("right", col.transform);
+            right = normalize(dis, 0, 0.67f); //-0.5
+        }
+        else if (col.gameObject.name.Equals("Top"))
+        {
+            float dis = getDistance("top", col.transform);
+            up = normalize(dis, 0, 0.61f);//-0.6
+        }
+        else if (col.gameObject.name.Equals("Bottom"))
+        {
+            float dis = getDistance("bottom", col.transform);
+            down = normalize(dis, 0, 0.40f); //-0.6
+        }
+        else if (col.gameObject.name.Equals("Forward"))
+        {
+            float dis = getDistance("forward", col.transform);
+            forward = normalize(dis, 0f, 0.35f); //-0.8
+        }
+        tv.text = "Left: " + left.ToString("F2") + "\nRight: " + right.ToString("F2") + "\nTop: " + up.ToString("F2") + "\nDown: " + down.ToString("F2") + "\nForward: " + forward.ToString("F2");
+        PlayerPrefs.SetFloat("Left", left);
+        PlayerPrefs.SetFloat("Right", right);
+        PlayerPrefs.SetFloat("Up", up);
+        PlayerPrefs.SetFloat("Down", down);
+        PlayerPrefs.SetFloat("Forward", forward);
+
+    }
+    private float getDistance(string dir, Transform collision)
+    {
+        Vector3 vect = cubeParent.position - collision.position;
+        
+        switch (dir)
+        {
+            case "left":
+                vect.z = 0;
+                vect.y = 0;
+                break;
+            case "right":
+                vect.z = 0;
+                vect.y = 0;
+                break;
+            case "bottom":
+                vect.z = 0;
+                vect.x = 0;
+                break;
+            case "forward":
+                vect.x = 0;
+                vect.y = 0;
+                break;
+            case "top":
+                vect.z = 0;
+                vect.x = 0;
+                break;
+        }
+        return vect.magnitude;
+    }
+    
+   private float normalize(float value, float min, float oldMax)
+    {
+        return (((value - min) * (1 - 0)) / (oldMax - min)) + 0;
+    }
+   
+
+}
